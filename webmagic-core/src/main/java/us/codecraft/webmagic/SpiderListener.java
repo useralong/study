61cb158b86dafef462ae6c8bcb767396997501cb
useralong <1965397740@qq.com>
2022-08-29 17:02:39 +0800

    learned item

---
+++ webmagic-core/src/main/java/us/codecraft/webmagic/SpiderListener.java
@@ -0,0 +1,23 @@
+package us.codecraft.webmagic;
+
+/**
+ * Listener of Spider on page processing. Used for monitor and such on.
+ *
+ * @author code4crafer@gmail.com
+ * @since 0.5.0
+ */
+public interface SpiderListener {
+
+    public void onSuccess(Request request);
+
+    /**
+     * @deprecated Use {@link #onError(Request, Exception)} instead.
+     */
+    @Deprecated
+    public void onError(Request request);
+
+    default void onError(Request request, Exception e) {
+        this.onError(request);
+    }
+
+}
